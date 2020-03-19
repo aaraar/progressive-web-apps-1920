c0a5a4d4bbf8aa7104c7fd792902d83197cad00d
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2020-03-19 19:34:29 +0100

    Fixed build error

--- data/generateData.js
+++ data/generateData.js
@@ -1,45 +1,56 @@
-const fetch = require ( 'node-fetch' );
-const path = require ( 'path' );
-const fs = require ( 'fs-extra' );
-require ( 'dotenv' ).config ();
+const fetch = require('node-fetch');
+const path = require('path');
+const fs = require('fs-extra');
+require('dotenv').config();
 
-const stationsPath = path.join ( __dirname, '..', 'public/data/stations.json' );
-const placesPath = path.join ( __dirname, '..', 'public/data/places.json' );
+const stationsPath = path.join(__dirname, '..', 'public/data/stations.json');
+const placesPath = path.join(__dirname, '..', 'public/data/places.json');
 
 
-function writeToFile( writePath, data, cb ) {
-    fs.mkdirp ( path.dirname ( writePath ), err => {
-        if ( err ) console.error ( err );
-        fs.writeFile ( writePath, data, { flag: 'w' }, cb )
-    } );
+function writeToFile(writePath, data, cb) {
+    fs.mkdirp(path.dirname(writePath), err => {
+        if (err) console.error(err);
+        fs.writeFile(writePath, data, {flag: 'w'}, cb)
+    });
 }
 
-function checkFile( writePath, mode = 'json' ) {
-    return new Promise ( ( resolve, reject ) => {
-        fs.readFile ( writePath, 'utf-8', ( err, data ) => {
-            if ( err ) reject ( err );
-            if ( mode === 'json' ) console.log ( `🛤 🔍 Data tracks constructed at ${ writePath }` );
+function checkFile(writePath, mode = 'json') {
+    return new Promise((resolve, reject) => {
+        fs.readFile(writePath, 'utf-8', (err, data) => {
+            if (err) reject(err);
+            if (mode === 'json') console.log(`🛤 🔍 Data tracks constructed at ${writePath}`);
             // else if ( mode === 'html' ) console.log ( `🛤 📄 Markup tracks constructed at  ${ writePath }` );
             // else console.log ( `👮 Are you spraying graffiti at ${ writePath } ?` );
-            resolve ( data );
-        } );
-    } )
+            resolve(data);
+        });
+    })
 }
 
-function generateJson( writePath, dataPromise ) {
-    return new Promise ( ( resolve, reject ) => {
-        dataPromise ().then ( ( json ) => {
-            writeToFile ( writePath, JSON.stringify ( json ), ( err ) => {
-                if ( err ) return console.error ( err );
-                checkFile ( writePath ).then ( () => resolve () ).catch ( err => reject ( err ) )
-            } );
-        } );
-    } )
+function generateJson(writePath, dataPromise) {
+    return new Promise((resolve, reject) => {
+        dataPromise().then((json) => {
+            writeToFile(writePath, JSON.stringify(json), (err) => {
+                if (err) return console.error(err);
+                checkFile(writePath).then(() => resolve()).catch(err => reject(err))
+            });
+        });
+    })
 }
+
+
+function getJson(writePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(writePath, 'utf8', (err, data) => {
+            if (err) reject(err);
+            resolve(JSON.parse(data));
+        })
+    })
+}
+
 
 function getStations() {
-    return new Promise ( ( resolve, reject ) => {
-        fetch (
+    return new Promise((resolve, reject) => {
+        fetch(
             'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations',
             {
                 method: 'GET',
@@ -47,20 +58,22 @@ function getStations() {
                     'X-Requested-With': 'XMLHttpRequest',
                     'Ocp-Apim-Subscription-Key': process.env.API_KEY_STATIONS
                 }
-            } )
-            .then ( ( res ) => {
-                if ( res.ok ) return res.json ();
-                else reject ( res );
-            } )
-            .then ( json => {
-                const mappedStations = mapStations ( json.payload );
+            })
+            .then((res) => {
+                if (res.ok) return res.json();
+                else reject(res);
+            })
+            .then(json => {
                 console.log('🚀 Basic Station info created');
-            } )
-    } )
+                resolve(mapStations(json.payload));
+            })
+            .catch(err => console.error(err))
+
+    })
 }
 
 function getPlaces() {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fetch(
             `https://gateway.apiportal.ns.nl/places-api/v2/places?lang=en&details=True`,
             {
@@ -71,20 +84,20 @@ function getPlaces() {
                 }
             }
         )
-            .then ( ( res ) => {
-                if ( res.ok ) return res.json ();
-                else reject ( res );
-            } )
-            .then ( json => {
-                resolve (json.payload)
-            } )
+            .then((res) => {
+                if (res.ok) return res.json();
+                else reject(res);
+            })
+            .then(json => {
+                resolve(json.payload)
+            })
             .catch(err => console.error(err))
     });
 }
 
 function getArrivals(code) {
-    return new Promise ( ( resolve, reject ) => {
-        fetch (
+    return new Promise((resolve, reject) => {
+        fetch(
             `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/arrivals?station=${code}`,
             {
                 method: 'GET',
@@ -92,34 +105,26 @@ function getArrivals(code) {
                     'X-Requested-With': 'XMLHttpRequest',
                     'Ocp-Apim-Subscription-Key': process.env.API_KEY_STATIONS
                 }
-            } )
-            .then ( ( res ) => {
-                if ( res.ok ) return res.json ();
-                else reject ( res );
-            } )
-            .catch( err => { console.error( err ) } )
-            .then ( json => {
-                resolve (json.payload.arrivals );
-            } )
-    } )
+            })
+            .then((res) => {
+                if (res.ok) return res.json();
+                else reject(res);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            .then(json => {
+                resolve(json.payload.arrivals);
+            })
+    })
 }
 
 
-function getJson(writePath) {
-    return new Promise ( ( resolve, reject ) => {
-        fs.readFile ( writePath, 'utf8', ( err, data ) => {
-            if ( err ) reject ( err );
-            resolve ( JSON.parse ( data ) );
-        } )
-    } )
-}
-
-
-function mapStations( stations ) {
-    return stations.map ( station => {
+function mapStations(stations) {
+    return stations.map(station => {
         station.name = station.namen.lang;
         station.countryCode = station.land;
-        switch ( station.land ) {
+        switch (station.land) {
             case 'ERROR':
                 station.country = 'Try redefining your search';
                 break;
@@ -146,7 +151,7 @@ function mapStations( stations ) {
                 break;
         }
         return station;
-    } );
+    });
 }
 
 module.exports = {
